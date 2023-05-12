6d89b6c3d6e9555d0854be2ee0f3da9761cfd8ed
avganka <karambasterg@gmail.com>
2023-05-12 12:06:56 +0300

    Добавлена функция по форматировани времени

--- index.js
+++ index.js
@@ -7,13 +7,29 @@ const createTimerAnimator = () => {
     const interval = setInterval(() => {
       if (seconds <= 0) {
         clearInterval(interval);
-        timerEl.textContent = 0;
+        timerEl.textContent = formatTime(0);
       } else {
-        timerEl.textContent = seconds;
+        timerEl.textContent = formatTime(seconds);
       }
       seconds -= 1;
     }, 1000);
   };
+};
+
+// Функия для форматирования времени в формат hh:mm:ss
+const formatTime = (seconds) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  return `${addZeroToNumber(hours)}:${addZeroToNumber(minutes)}:${addZeroToNumber(
+    remainingSeconds
+  )}`;
+};
+
+// Вспомогательная функция для добавления 0 к числам, если они меньше 10
+const addZeroToNumber = (number) => {
+  return number < 10 ? '0' + number : number;
 };
 
 const animateTimer = createTimerAnimator();
